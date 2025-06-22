79a419d2d6ac680f1813fc2d804b741ff0536d09
sarunas.gincas <sarunikss@gmail.com>
2025-06-23 01:57:32 +0300

    feat: add minimal trait implementation

--- packages/jsii-pacmak/lib/targets/rust/types/trait.ts
+++ packages/jsii-pacmak/lib/targets/rust/types/trait.ts
@@ -1,11 +1,17 @@
 import { CodeMaker } from "codemaker";
 import { RustType } from "../rust-type";
-import { InterfaceType } from "jsii-reflect";
+import { InterfaceType, Method } from "jsii-reflect";
 
 export class RustTrait extends RustType<InterfaceType> {
     public emit(code: CodeMaker) {
         code.openBlock(`pub trait ${this.type.name}`);
-        // Emit tait methods here
+
+        Object.keys(this.type.getMethods()).forEach((name: string) => {
+            code.openBlock(`fn ${name}(&self)`);
+            code.line(`todo!();`);
+            code.closeBlock();
+        });
+
         code.closeBlock();
     }
 }
