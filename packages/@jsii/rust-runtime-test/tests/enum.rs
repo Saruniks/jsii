7b81ffe632c9ee3dd2d9288ea8f5bdcd0139a576
sarunas.gincas <sarunikss@gmail.com>
2025-07-03 11:12:28 +0300

    fix: remove comments

--- packages/@jsii/rust-runtime-test/tests/enum.rs
+++ packages/@jsii/rust-runtime-test/tests/enum.rs
@@ -15,13 +15,9 @@ fn test_string_like_enum() {
 fn test_integer_like_enum() {
     // It's a lie from jsii developers that it's random
     let res = EnumDispenser::random_integer_like_enum();
-    // TODO: No language actually have a capability to get discriminant value of enum?
     assert_eq!(res, AllTypesEnum::YourEnumValue);
-    // assert_eq!(res as i64, 100);
     let res = EnumDispenser::random_integer_like_enum();
     assert_eq!(res, AllTypesEnum::YourEnumValue);
-    // assert_eq!(res as i64, 100);
     let res = EnumDispenser::random_integer_like_enum();
     assert_eq!(res, AllTypesEnum::YourEnumValue);
-    // assert_eq!(res as i64, 100);
 }
